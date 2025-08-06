0810f80f1b48e4d2ec70cb7e2a7e4d420b6a01cd
zhudiana <yoditfentaw90@gmail.com>
2025-08-06 11:57:45 +0200

    title changed

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import UploadPage from "./pages/UploadPage";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "Hexify";
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
